f4be216e778bed8d48bfdd8688a66330a73b122c
anhtruong1209 <sdtla0911114819@gmail.conm>
2023-07-27 15:09:12 +0700

    update logic

--- tests/DAFlow/test_SDAFNet_viton.py
+++ tests/DAFlow/test_SDAFNet_viton.py
@@ -15,72 +15,48 @@ from utils.utils import AverageMeter
 from torchvision import transforms, utils
 from torch.utils import data
 from torchvision.utils import save_image
+import base64
+
 cudnn.benchmark = True
 
-
-
-def get_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--name', type=str, required=True)
-    parser.add_argument('-b', '--batch_size', type=int, default=4)
-    parser.add_argument('--lr', type=float, default=0.0004)
-    parser.add_argument('-j', '--workers', type=int, default=4)
-    parser.add_argument('--load_height', type=int, default=256)
-    parser.add_argument('--load_width', type=int, default = 192)
-    parser.add_argument('--mode', type=str, default='test')
-    parser.add_argument('--shuffle', action='store_true')
-    parser.add_argument('--add_compare', action='store_true')
-    parser.add_argument('--dataset_dir', type=str, default='./data')
-    parser.add_argument('--dataset_imgpath', type=str, default='VITON/VITON_test')
-    parser.add_argument('--dataset_list', type=str, default='VITON/test_unpairs.txt')
-    parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints/')
-    parser.add_argument('--save_dir', type=str, default='./results/')
-    parser.add_argument('--multi_flows', type=int, default=6)
-    parser.add_argument('--display_freq', type=int, default=100)
-    opt = parser.parse_args()
-    return opt
-
-def test(opt, net):
-    test_dataset = VITONDataset(opt)
-    test_loader = data.DataLoader(test_dataset, batch_size=opt.batch_size, shuffle=opt.shuffle,num_workers=opt.workers)
-    with torch.no_grad():
-        for i, inputs in enumerate(tqdm.tqdm(test_loader)):
-            img_names = inputs['img_name']
-            cloth_names = inputs['c_name']['paired']
-            img = inputs['img']
-            img_agnostic = inputs['img_agnostic'] #Masked model image
-            pose = inputs['pose']
-            cloth_img = inputs['cloth']['paired']
-            img =  F.interpolate(img, size=(256, 192), mode='bilinear')
-            cloth_img = F.interpolate(cloth_img, size=(256, 192), mode='bilinear')
-            img_agnostic = F.interpolate(img_agnostic, size=(256, 192), mode='bilinear')
-            pose = F.interpolate(pose, size=(256, 192), mode='bilinear')
-            ref_input = torch.cat((pose, img_agnostic), dim=1)
-            tryon_result = net(ref_input, cloth_img, img_agnostic).detach()
-            for j in range(tryon_result.shape[0]):
-                save_image(tryon_result[j:j+1], os.path.join(opt.save_dir, opt.name, "vis_viton_out", img_names[j]), nrow=1, normalize=True, range=(-1,1))
-            if opt.add_compare:
-                tryon_result = torch.cat([img_agnostic, cloth_img, tryon_result],2)
-                save_image(tryon_result, os.path.join(opt.save_dir, opt.name, "vis_viton_out", img_names[0]), nrow=10, normalize=True, range=(-1,1))
-
+def test(net):
+    
+    img_name= ['000001_0.jpg']
+    c_name= ['001744_1.jpg']
+    img_base64 = ''
+    test_dataset = VITONDataset(img_name, c_name)
+    test_loader = data.DataLoader(test_dataset, batch_size=4, shuffle=True,num_workers=4)
+    for i, inputs in enumerate(tqdm.tqdm(test_loader)):
+        img_names = inputs['img_name']
+        cloth_names = inputs['c_name']['paired']
+        img = inputs['img']
+        img_agnostic = inputs['img_agnostic'] #Masked model image
+        pose = inputs['pose']
+        cloth_img = inputs['cloth']['paired']
+        img =  F.interpolate(img, size=(256, 192), mode='bilinear')
+        cloth_img = F.interpolate(cloth_img, size=(256, 192), mode='bilinear')
+        img_agnostic = F.interpolate(img_agnostic, size=(256, 192), mode='bilinear')
+        pose = F.interpolate(pose, size=(256, 192), mode='bilinear')
+        ref_input = torch.cat((pose, img_agnostic), dim=1)
+        tryon_result = net(ref_input, cloth_img, img_agnostic).detach()
+        for j in range(tryon_result.shape[0]):
+            save_image(tryon_result[j:j+1], os.path.join('./results/', 'TEST_UNPAIR', "vis_viton_out", img_names[j]), nrow=1, normalize=True, range=(-1,1))
+            with open(f'./results/TEST_UNPAIR/vis_viton_out/{img_names[j]}', "rb") as img_file:
+                my_string = base64.b64encode(img_file.read())
+          
 def main():
-    
-    # import gdown
-
-    # url = 'https://drive.google.com/file/d/1N_guN04Kxg4cFN6qHSu5YYIp52n82zOD/view?usp=sharing'
-    # output = 'checkpoints/ckpt_viton.pt'
-    # gdown.download(url, output, quiet=False)
-    
-    opt = get_opt()
-    if not os.path.exists(os.path.join(opt.save_dir, opt.name, "vis_viton_out")):
-        os.makedirs(os.path.join(opt.save_dir, opt.name,"vis_viton_out"))
+    name = 'TEST_UNPAIR'
+    save_dir = './results/'
+    if not os.path.exists(os.path.join(save_dir, name, "vis_viton_out")):
+        os.makedirs(os.path.join(save_dir, name,"vis_viton_out"))
+        
     sdafnet = SDAFNet_Tryon(ref_in_channel=6)
     sdafnet.load_state_dict(torch.load("checkpoints/ckpt_viton.pt", map_location='cpu'))
     sdafnet.eval()
     
     import time
     start = time.time()
-    test(opt, sdafnet)
+    test(sdafnet)
     end = time.time() - start
     
     print(end)
