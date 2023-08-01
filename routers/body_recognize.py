34375c25b10aaccee59129ae26d3cd55f7cba02d
anhtruong1209 <sdtla0911114819@gmail.conm>
2023-08-01 23:35:26 +0700

    addd logic

--- routers/body_recognize.py
+++ routers/body_recognize.py
@@ -9,11 +9,12 @@ from models.request import convert_multis_image
 import traceback
 from PIL import Image
 from io import BytesIO
+from services.clothesviton import reference
 router = APIRouter()
+
 # VITON CLOTHES
 @router.post("/viton_clothes")    
 def body_background(image: dict = Depends(convert_multis_image)):
-    
     image_byte1 = image['image1']
     image_byte2 = image['image2']
     
@@ -27,16 +28,13 @@ def body_background(image: dict = Depends(convert_multis_image)):
     image2 = Image.open(BytesIO(image_byte2))
     image_name1 = image["image_name1"]
     image_name2 = image["image_name2"]
-    img_path1 = 'data/VITON/VITON_test/test_img/' + image_name1
+    img_path1 = 'services/clothesviton/data/VITON/VITON_test/test_img/' + image_name1
     image1.save(img_path1)
-    img_path2 = 'data/VITON/VITON_test/clothes/' + image_name2
+    print("Image saved 1")
+    img_path2 = 'services/clothesviton/data/VITON/VITON_test/clothes/' + image_name2
     image2.save(img_path2)
+    print("Image saved 2")
     
-    # nparr_image = np.fromstring(image_byte1, np.uint8)
-    # human_image = cv2.imdecode(nparr_image, cv2.IMREAD_COLOR)
-    
-    # nparr_background = np.fromstring(image_byte2, np.uint8)
-    # glass_image = cv2.imdecode(nparr_background, cv2.IMREAD_COLOR)
-
-    # return ResponeModel(image=image_byte1)
+    a =  reference.reference([image_name1],[image_name2])
+    return ResponeModel(image=a)
 
